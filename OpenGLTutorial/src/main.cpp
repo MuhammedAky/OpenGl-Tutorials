8e72ad235f55b8b2b8691fef53e7b1f597725fea
MuhammedAky <muhammedaky24@gmail.com>
2022-10-04 08:49:34 +0300

    Transitions & Shader

--- OpenGLTutorial/src/main.cpp
+++ OpenGLTutorial/src/main.cpp
@@ -1,75 +1,141 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <fstream>
+#include <sstream>
+#include <streambuf>
+#include <string>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+
+#include "Shader.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-
+std::string loadShaderSrc(const char* filename);
 
 int main() {
-	std::cout << "Hello, Test!" << std::endl;
-	
-	// glm test
-	glm::vec4 vec(1.0f, 1.0f, 1.0f, 1.0f);
-	glm::mat4 trans = glm::mat4(1.0f); // identity matrix
-	//trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f)); // translation
-	//trans = glm::rotate(trans, glm::radians(90.f), glm::vec3(0.0, 0.0, 1.0f)); // rotation
-	trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
-	vec = trans * vec;
+	int success;
+	char infoLog[512];
 
-	std::cout << vec.x << " " << vec.y << " " << vec.z << std::endl;
-
+	std::cout << "Hello, world!" << std::endl;
 
 	glfwInit();
+
+	// openGL version 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+# ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COPMPAT, GL_TRUE);
+#endif
 
-	GLFWwindow* window = glfwCreateWindow(800, 300, "OpenglTutorial", NULL, NULL);
-
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Tutorial", NULL, NULL);
 	if (window == NULL) { // window not created
 		std::cout << "Could not create window." << std::endl;
 		glfwTerminate();
-
 		return -1;
 	}
-
 	glfwMakeContextCurrent(window);
-
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		glfwTerminate();
-
 		return -1;
 	}
 
 	glViewport(0, 0, 800, 600);
 
-
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// SHADERS===============================
+	Shader shader("assets/vertex_core.glsl", "assets/fragment_core.glsl");
+
+	float vertices[] = {
+		// positions		// colors
+		-0.5f, -0.5f, 0.0f,	1.0f, 1.0f, 0.5f,	// bottom left
+		-0.5f, 0.5f, 0.0f,	0.5f, 1.0f, 0.75f,	// top left
+		0.5f, -0.5f, 0.0f,	0.6f, 1.0f, 0.2f,	// bottom right
+		0.5f, 0.5f, 0.0f,	1.0f, 0.2f, 1.0f	// top right
+	};
+	unsigned int indices[] = {
+		0, 1, 2, // first triangle
+		3, 1, 2  // second triangle
+	};
+
+	// VBO, VAO, EBO
+	unsigned int VBO, VAO, EBO;
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &EBO);
+
+	// bind VAO
+	glBindVertexArray(VAO);
+
+	// bind VBO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// put index array in EBO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// set attributes pointers
+	// position
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// color
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glm::mat4 trans = glm::mat4(1.0f);
+	trans = glm::rotate(trans, glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	trans = glm::scale(trans, glm::vec3(0.5f, 1.5f, 0.5f));
+	shader.activate();
+	shader.setMat4("transform", trans);
 
 	while (!glfwWindowShouldClose(window)) {
 		// process input
 		processInput(window);
 
 		// render
-		glClearColor(0.2f, 0.3f,0.3f,1.0f);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// draw shapes
+		glBindVertexArray(VAO);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		shader.activate();
+		// set color
+		float timeValue = glfwGetTime();
+		float blueValue = (sin(timeValue) / 2.0f) + 0.5f;
+		shader.set4Float("ourColor", 0.0f, 0.0f, blueValue, 1.0f);
+		trans = glm::rotate(trans, glm::radians(timeValue / 100), glm::vec3(0.1f, 0.1f, 0.1f));
+		shader.setMat4("transform", trans);
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		trans = glm::translate(trans, glm::vec3(0.5f, 0.5f, 0.0f));
+		shader.setMat4("transform", trans);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		trans = glm::translate(trans, glm::vec3(-0.5f, -0.5f, 0.0f));
+		shader.setMat4("transform", trans);
+
+		glBindVertexArray(0);
 
 		// send new frame to window
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteVertexArrays(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+
 	glfwTerminate();
-
-
 	return 0;
 }
 
@@ -82,5 +148,3 @@ void processInput(GLFWwindow* window) {
 		glfwSetWindowShouldClose(window, true);
 	}
 }
-
-
